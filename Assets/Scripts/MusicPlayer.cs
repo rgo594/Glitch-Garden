479083d51d93041b701a77f17b525819f0553875
Rob O'Gara <rgo594@gmail.com>
2020-12-27 11:49:47 -0500

    Tower Defense Game

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+    private void SetUpSingleton()
+    {
+        if (1 < FindObjectsOfType(GetType()).Length)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    Scene scene;
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        if (scene.buildIndex == 0)
+        {
+            StartCoroutine(LoadStartScreen());
+        }
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+
+    IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(4);
+        LoadNextScene();
+    }
+}
