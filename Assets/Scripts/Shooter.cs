3fa225caf271fdd33528b9547dc4ef0e5ea2a217
Rob O'Gara <rgo594@gmail.com>
2021-01-01 10:30:17 -0500

    Fixed Lane Positions and Defender Colliders

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,10 +5,60 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log("defender position is" + transform.position);
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+            //TODO change animation state to idle
+        }
+    }
 
     public void Fire()
     {
-        FindObjectOfType<GameObject>();
+
         Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+
+    private bool IsAttackerInLane()
+    {
+        //think if lane is 0 it bugs
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough =
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
     }
 }
